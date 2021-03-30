38c3b7b502b2a2ea6846f1d035b9882ae9a155f6
Sandeep Kandula <sandeep.kandula@in.abb.com>
2021-03-30 20:09:36 +0530

    Connectivity with Azure PostgreSQL.

---
+++ 2/PostgreSQL/SamuraiApp/SamuraiApp.Domain/Samurai.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace SamuraiApp.Domain
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Using object initializer
+        //One to many relationships
+        //Trying to avoid quotes to null list
+        public List<Quote> Quotes { get; set; } = new();
+    }
+}
